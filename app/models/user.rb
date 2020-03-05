9e232f9774f3d85d76e4ae2fdfb23941edebca98
Valentine <valentinemaillard1@gmail.com>
2020-03-05 11:30:43 -0500

    added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, uniqueness: true
+    validates :username, acceptance: true
+    validates :first_name, acceptance: true
+    validates :last_name, acceptance: true
+    validates :password, acceptance: true
+    validates :address, acceptance: true
+    validates :email, acceptance: true
 
     has_many :reviews, dependent: :destroy
     has_many :items, through: :reviews
