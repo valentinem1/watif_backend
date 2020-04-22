3d316347928acd1ba2012f1314a5bb2f9913a642
Valentine <valentinemaillard1@gmail.com>
2020-04-22 16:14:18 -0400

    deleted unecessary comments and modified the create method for cart_joiner

--- app/controllers/cart_joiners_controller.rb
+++ app/controllers/cart_joiners_controller.rb
@@ -13,56 +13,12 @@ class CartJoinersController < ApplicationController
     end
 
     def create
-        # find the item in the logged_user cart_joiner 
-        # logged_user.cart.cart_joiners.any?{|h| h[:item_id] === params[:item_id]} ==> true
-        #  if true increment quantity
-        # else return the item
-        # byebug
-
-        # find the item in the logged_user cart_joiner
-        # if it exist count how many times it is in cart_joiner
-
-        logged_user_cart_joiner = logged_user.cart.cart_joiners
-        # byebug
-        item = logged_user_cart_joiner.find_by(item_id: params[:item_id])
+        added_item = Item.find(params[:item_id])
+        new_item_quantity = added_item.quantity - 1
+        added_item.update(quantity: new_item_quantity)
         
-        if item.nil? 
-            new_item_quantity = item.quantity - 1
-            item.update(quantity: new_item_quantity)
-            
-            create_cart_joiner = cart_joiner_params.merge({cart_id: logged_user.cart.id})
-            cart_joiner = CartJoiner.create(create_cart_joiner)
-        else
-            nil
-        end
-
-        # render json: add_joiner
-
-
-        # @added_item = Item.find(params[:item_id])
-        
-        # @new_item_quantity = @added_item.quantity - 1
-        # @added_item.update(quantity: @new_item_quantity)
-        
-        # create_cart_joiner = cart_joiner_params.merge({cart_id: logged_user.cart.id})
-        # @cart_joiner = CartJoiner.create(create_cart_joiner)
-
-        # functionality to increase quantity in cart without duplicating an item
-        # check_item = logged_user.cart.cart_joiners.any?{|cart_joiner| cart_joiner[:item_id] === params[:item_id]}
-        
-        # if(check_item)
-
-        #     @cart_joiner = CartJoiner.find_by(item_id: params[:item_id])
-            
-        #     @new_cart_quantity = @cart_joiner.quantity + 1
-        #     @cart_joiner.update(quantity: @new_cart_quantity)
-            
-        #     render json: @cart_joiner
-        # else
-        #     create_cart_joiner = cart_joiner_params.merge({cart_id: logged_user.cart.id})
-        #     @cart_joiner = CartJoiner.create(create_cart_joiner)
-
-        # end
+        create_cart_joiner = cart_joiner_params.merge({cart_id: logged_user.cart.id})
+        cart_joiner = CartJoiner.create(create_cart_joiner)
         
         render json: cart_joiner
     end
